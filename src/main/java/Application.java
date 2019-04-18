4c02793214dcc8ffcaf5dc961537f2bac311ad91
Hemant Batra <hemant.batra@theinnovationinc.co>
2019-04-18 23:11:54 +0530

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -13,7 +13,7 @@ public class Application {
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Example.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
