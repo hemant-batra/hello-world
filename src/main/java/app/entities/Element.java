24483bf5ce55a2e880dd1927240d0b5c89a6f80f
hemant-batra <hemant.batra@theinnovationinc.co>
2019-04-26 23:00:15 +0530

    heroku

--- src/main/java/app/entities/Element.java
+++ src/main/java/app/entities/Element.java
@@ -1,11 +1,14 @@
 package app.entities;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+@Data
 @Entity
 @Table(name = "elements")
 public class Element implements Serializable {
