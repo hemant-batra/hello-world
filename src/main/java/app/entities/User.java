b74cb22255ba6f415ae467a284ab9c4bbd073d79
hemant-batra <hemant.batra@theinnovationinc.co>
2019-04-27 23:21:45 +0530

    heroku

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -3,12 +3,14 @@ package app.entities;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
     private String userId;
     private String ipAddress;
     private String userName;
