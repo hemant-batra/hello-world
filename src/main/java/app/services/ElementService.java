45fd7cc66abf1b919d50002c0bcdc7fdca25c836
hemant-batra <hemant.batra@theinnovationinc.co>
2019-04-27 21:20:25 +0530

    heroku

--- src/main/java/app/services/ElementService.java
+++ src/main/java/app/services/ElementService.java
@@ -3,32 +3,19 @@ package app.services;
 import app.dtos.ElementDTO;
 import app.entities.Element;
 import app.repositories.ElementsRepository;
-import app.utilities.Converter;
+import app.utilities.EntityConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class ElementService extends Converter<Element, ElementDTO> {
+public class ElementService extends EntityConverter<String, Element, ElementDTO> {
 
     private final ElementsRepository elementsRepository;
 
     @Autowired
     public ElementService(ElementsRepository elementsRepository) {
-        super(Element.class, ElementDTO.class);
+        super(elementsRepository, Element.class, ElementDTO.class);
         this.elementsRepository = elementsRepository;
     }
 
-    public List<ElementDTO> findAll() {
-        return toDTOs(elementsRepository.findAll());
-    }
-
-    public ElementDTO findOne(String id) {
-        return toDTO(elementsRepository.findOne(id));
-    }
-
-    public ElementDTO save(ElementDTO elementDTO) {
-        return toDTO(elementsRepository.save(toEntity(elementDTO)));
-    }
 }
--- src/main/java/app/utilities/EntityConverter.java
+++ src/main/java/app/utilities/EntityConverter.java
@@ -2,18 +2,22 @@ package app.utilities;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.io.Serializable;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
-public class Converter<Entity, DTO> {
+public class EntityConverter<Id extends Serializable, Entity, DTO> {
 
+    private JpaRepository<Entity, Id> jpaRepository;
     private Class<Entity> entityClass;
     private Class<DTO> dtoClass;
     private ObjectMapper mapper;
 
-    protected Converter(Class<Entity> entityClass, Class<DTO> dtoClass) {
+    protected EntityConverter(JpaRepository<Entity, Id> jpaRepository, Class<Entity> entityClass, Class<DTO> dtoClass) {
+        this.jpaRepository = jpaRepository;
         this.entityClass = entityClass;
         this.dtoClass = dtoClass;
         mapper = new ObjectMapper();
@@ -43,4 +47,33 @@ public class Converter<Entity, DTO> {
                 .map(this::toEntity)
                 .collect(toList());
     }
+
+    public List<DTO> findAll() {
+        return toDTOs(jpaRepository.findAll());
+    }
+
+    public DTO findOne(Id id) {
+        return toDTO(jpaRepository.findOne(id));
+    }
+
+    public List<DTO> findAll(List<Id> ids) {
+        return toDTOs(jpaRepository.findAll(ids));
+    }
+
+    public DTO save(DTO dto) {
+        return toDTO(jpaRepository.save(toEntity(dto)));
+    }
+
+    public List<DTO> save(List<DTO> dtoList) {
+        return toDTOs(jpaRepository.save(toEntities(dtoList)));
+    }
+
+    public void delete(DTO dto) {
+        jpaRepository.delete(toEntity(dto));
+    }
+
+    public void delete(List<DTO> dtoList) {
+        jpaRepository.delete(toEntities(dtoList));
+    }
+
 }
