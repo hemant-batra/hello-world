69fc7a0a959d57febd5f14ab1695ed95a9204aaa
hemant-batra <hemant.batra@theinnovationinc.co>
2019-04-29 01:22:19 +0530

    heroku

--- src/main/java/app/dtos/CreateElementDTO.java
+++ src/main/java/app/dtos/CreateElementDTO.java
@@ -3,14 +3,10 @@ package app.dtos;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import java.sql.Timestamp;
-
 @Data
 public class CreateElementDTO {
-    private String elementId;
     private String ipAddress;
     private String userName;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Kolkata")
-    private Timestamp createdOn;
     private String content;
 }
