93b7c7e7e7724660d89941b182f1ec86a660395b
hemant-batra <hemant.batra@theinnovationinc.co>
2019-04-29 13:03:51 +0530

    heroku

--- src/main/java/app/controller/Controller.java
+++ src/main/java/app/controller/Controller.java
@@ -29,7 +29,7 @@ public class Controller {
     }
 
     @GetMapping("elements")
-    public List<ElementDTO> getAllElements(@RequestParam("createdOn") String strCreatedOn) {
+    public List<ElementDTO> getAllElements(@RequestParam(name = "createdOn", required = false) String strCreatedOn) {
         return flow.getAllElements(strCreatedOn);
     }
 
