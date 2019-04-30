1b4863f3b7b00c366affa5291405d3b57f35ae1f
hemant-batra <hemant.batra@theinnovationinc.co>
2019-04-30 22:17:07 +0530

    heroku

--- src/main/java/app/jpa/entities/Element.java
+++ src/main/java/app/jpa/entities/Element.java
@@ -1,5 +1,6 @@
 package app.jpa.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.Entity;
@@ -16,6 +17,7 @@ public class Element implements Serializable {
     @Id
     private String elementId;
     private String ipAddress;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd HH:mm:ss", timezone = "Asia/Kolkata")
     private Timestamp createdOn;
     private String content;
 }
